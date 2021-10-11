807377489afbcb540fb9b8cac4d9b7981ddade19
ChancelorReese <Chancelor.Reese@gmail.com>
2021-10-11 10:46:23 -0600

    new

--- 05-jumper/jumper/game/__init__.py
+++ 05-jumper/jumper/game/__init__.py
@@ -1,6 +1,6 @@
 """
 The game package contains specific classes for playing Jumper.
-"""
 print("Hello everyone. How long do you think it will take for Jacob to see this message.")
 print("Hello, It will probably take a very long time")
 print("Jacob, when you see this please state the date and time of seeing this message!")
+"""
