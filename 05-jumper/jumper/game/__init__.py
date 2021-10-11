5f524bbb8595cbbad1c91893b6a1605a1fc013ef
mickfish1996 <fis17005@byui.eud>
2021-10-11 10:45:14 -0600

    Grammer

--- 05-jumper/jumper/game/__init__.py
+++ 05-jumper/jumper/game/__init__.py
@@ -3,4 +3,4 @@ The game package contains specific classes for playing Jumper.
 """
 print("Hello everyone. How long do you think it will take for Jacob to see this message.")
 print("Hello, It will probably take a very long time")
-print("Jacob, wehn you see this please state the date and time of seeing this message!")
+print("Jacob, when you see this please state the date and time of seeing this message!")
