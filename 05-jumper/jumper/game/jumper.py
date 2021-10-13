daf40b4b5226ef0765eadb8941fe41d55d138255
jhuffaker13 <91619777+jhuffaker13@users.noreply.github.com>
2021-10-13 10:57:52 -0600

    Some Jumper stuff

---
+++ 05-jumper/jumper/game/jumper.py
@@ -0,0 +1,10 @@
+class Jumper:
+    def store_jumper(self):
+        pass
+    
+    def cut_rope(self):
+        pass
+        rope = 4
+        rope -= 1
+        if rope == 0:
+            print("Holy crap, you killed him! You monster! Go to h*ck!")
