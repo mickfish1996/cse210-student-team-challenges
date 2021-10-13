a2dd7237c75436871fda23b4f96165e5500af0bb
mickfish1996 <fis17005@byui.edu>
2021-10-13 11:16:02 -0600

    updated console

--- 05-jumper/jumper/game/console.py
+++ 05-jumper/jumper/game/console.py
@@ -1,3 +1,13 @@
 
 class Console:
-    pass
+    def __init__(self):
+        pass
+
+    def board_out(self, board):
+        pass
+
+    def jumper_out(self,jumper):
+        pass
+
+    def prompt_user(self):
+        pass
