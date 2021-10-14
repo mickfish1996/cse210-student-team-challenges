fcc7a6cbf742f5bfc44255c809d9f0383486fa6c
mickfish1996 <fis17005@byui.edu>
2021-10-14 17:59:02 -0600

    updated set_word

--- 05-jumper/jumper/game/board.py
+++ 05-jumper/jumper/game/board.py
@@ -12,6 +12,8 @@ class Board():
     def set_word(self):
         num = random.randint(0, len(self.list))
         self.word = self.list[num]
+        for i in range (len(self.word)):
+            self.board_display.append("_ ")
 
     def get_word(self):
         return self.word
