02c09f9426ddbd99e9c68ecdeb74f7113ebc7726
mickfish1996 <fis17005@byui.edu>
2021-10-14 20:26:15 -0600

    added comments

--- 05-jumper/jumper/game/board.py
+++ 05-jumper/jumper/game/board.py
@@ -9,16 +9,19 @@ class Board():
         self.list = []
         self.jumper = Jumper
         
-    
+    # This method will get the word to use from a list
+    # that is read in
     def set_word(self):
         num = random.randint(0, len(self.list))
         self.word = self.list[num - 2]
         for i in range(len(self.word)):
             self.board_display.append("_ ")
-
+    
+    # This funciton will return a word to be used
     def get_word(self):
         return self.word
 
+    # This function will see if your guess is correct.
     def compare_word(self, guess):
         compare = list(self.word)
         check = False
@@ -29,7 +32,7 @@ class Board():
 
         return check
 
-
+    # This method will read the file
     def read_file(self):
         file = open("wordbank.txt", "r")
         self.list = file.readlines()
