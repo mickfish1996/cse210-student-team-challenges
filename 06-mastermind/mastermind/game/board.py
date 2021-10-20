92dda28c8edc3bf027c12a13fe52888dd3aa98ae
jhuffaker13 <91619777+jhuffaker13@users.noreply.github.com>
2021-10-20 10:59:13 -0600

    Basic in board.py

--- 06-mastermind/mastermind/game/board.py
+++ 06-mastermind/mastermind/game/board.py
@@ -1,3 +1,5 @@
+import random
+
 class Board():
     """
     Displays the board and gives the information to Console
@@ -16,4 +18,5 @@ class Board():
         text += ("{Player variable}'s turn:")
         return text
         
-    
+    def create_number(self):
+        guess_number = random.randint(1000, 9999)
