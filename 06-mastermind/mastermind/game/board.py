a3a05a33767059b6998c78789ee7526335fc1375
jhuffaker13 <91619777+jhuffaker13@users.noreply.github.com>
2021-10-20 10:52:57 -0600

    Rough draft of to_string

--- 06-mastermind/mastermind/game/board.py
+++ 06-mastermind/mastermind/game/board.py
@@ -2,3 +2,18 @@ class Board():
     """
     Displays the board and gives the information to Console
     """
+
+    def __init__(self):
+        """The class constructor"""
+        pass
+
+    def to_string(self):
+        """Converts the board data to string"""
+        text = ("----------------------")
+        text += ("Player {Player variable}: ----, ****")
+        text += ("Player {Player variable}: ----, ****")
+        text += ("----------------------")
+        text += ("{Player variable}'s turn:")
+        return text
+        
+    
