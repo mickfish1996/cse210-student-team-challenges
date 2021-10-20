3598d3ceb17d6b9c30378f17207515ba2968d96b
mickfish1996 <fis17005@byui.edu>
2021-10-20 11:00:43 -0600

    added prepare

--- 06-mastermind/mastermind/game/director.py
+++ 06-mastermind/mastermind/game/director.py
@@ -2,6 +2,7 @@ from game.board import Board
 from game.console import Console
 from game.player import Player
 from game.roster import Roster
+from game.move import Move
 
 import random
 
@@ -11,6 +12,17 @@ class Director():
     Director class
     """
     def __init__(self):
+        __board = Board
+        __console = Console
+        __roster = Roster
+        __move = Move
+        __prepare()
 
     def start_game():
         print("game")
+
+    def __prepare(self):
+        for i in range(2):
+            name = self.__roster.read(f"Enter a name for player {i + 1}: ")
+            player = player(name)
+            self.__console.add_player(player)
