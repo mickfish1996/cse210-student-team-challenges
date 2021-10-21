c52802c1c92b9ae756258c2f5e560df0d5f7732e
ChancelorReese <Chancelor.Reese@gmail.com>
2021-10-21 17:32:28 -0600

    discriptions of functions in console

--- 06-mastermind/mastermind/game/console.py
+++ 06-mastermind/mastermind/game/console.py
@@ -3,12 +3,15 @@ class Console():
     """
     console class
     """
-
+    
+    # Let user input names for the each of the 2 players
     def read(self,prompt):
         return input(prompt)
 
+    # Displays the board
     def display_board(self,text):
         print(text)
 
+    # prompt for the user input.
     def read_number(self, prompt):
         return int(input(prompt))
