b7ee230059670ae89f0b57dd04ae406cdf1110f6
ChancelorReese <Chancelor.Reese@gmail.com>
2021-10-21 17:42:20 -0600

    discriptions in move class

--- 06-mastermind/mastermind/game/move.py
+++ 06-mastermind/mastermind/game/move.py
@@ -3,9 +3,12 @@ class Move():
     """
     Move class. 
     """
+
+    # Initialization
     def __init__(self, guess):
         self._guess = guess
     
+    # Access prviate variable guess
     def get_move(self):
         return self._guess
         
