db48e461ad1f9b264d9c759c81c30091d9cc87ee
jhuffaker13 <91619777+jhuffaker13@users.noreply.github.com>
2021-10-21 17:37:13 -0600

    Changes to Board, including create_number

--- 06-mastermind/mastermind/game/board.py
+++ 06-mastermind/mastermind/game/board.py
@@ -7,19 +7,30 @@ class Board():
 
     def __init__(self):
         """The class constructor"""
-        self._guess = "----"
-        self._hing = "****"
+        self._guess = []
+        self._hint = []
+        self._guess_number = 0
+        self.create_number()
 
     def to_string(self, roster):
         """Converts the board data to string"""
         players = roster.get_players()
         text = ("----------------------")
-        text += (f"\nPlayer {players[0].get_name()}: {self._guess}, {self._hing}")
-        text += (f"\nPlayer {players[1].get_name()}: {self._guess}, {self._hing}")
+        text += (f"\nPlayer {players[0].get_name()}: {self._guess}, {self._hint}")
+        text += (f"\nPlayer {players[1].get_name()}: {self._guess}, {self._hint}")
         text += ("\n----------------------")
         return text
         
     def create_number(self):
-        guess_number = random.randint(1000, 9999)
+        self.guess_number = random.randint(1000, 9999)
+        for i in range(2):
+            self._guess[i] = "----"
+            self._hint[i] = "****"
 
-        
+    def compare(self, guess_number, guess):
+        pass
+        if guess_number == guess:
+            is_won() = True
+
+    def is_won():
+        pass
