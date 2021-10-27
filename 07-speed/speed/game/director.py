a86dde40c6396477410c614eeeff79147da11275
mickfish1996 <fis17005@byui.edu>
2021-10-27 11:13:35 -0600

    added to main

--- 07-speed/speed/game/director.py
+++ 07-speed/speed/game/director.py
@@ -1,4 +1,9 @@
 from game.score_board import Score_board
+from game.word import Word
+from game import constants
+from game.point import Point
+
 
 class director:
-    pass
+    def __init__(self):
+        self.
