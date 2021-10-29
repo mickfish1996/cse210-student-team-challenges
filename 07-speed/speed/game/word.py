ada76808b5271f84d0ab4acd8857bf0de54b01e4
mickfish1996 <fis17005@byui.edu>
2021-10-29 10:17:52 -0600

    added to word.py

--- 07-speed/speed/game/word.py
+++ 07-speed/speed/game/word.py
@@ -1,5 +1,18 @@
 from game.actor import Actor
+from game import constants
 
 class Word(Actor):
     def __init__(self):
         super().__init__()
+        self._point = 0
+        self.prepare()
+
+    def prepare(self):
+        list_size = len(LIBRARY)
+        num = random.randint(1,list_size - 1)
+        word = LIBRARY[num]
+
+        self.set_text(word.strip())
+        y = random.randint(1, MAX_Y)
+        position = Point(MAX_X, y)
+        self.set_position(position)
