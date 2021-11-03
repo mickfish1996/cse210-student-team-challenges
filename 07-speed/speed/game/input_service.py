5d399f57165f9e1fa24196c96c63edc4cc4c5c9a
mickfish1996 <fis17005@byui.edu>
2021-11-03 11:01:44 -0600

    now handles enter and escape

--- 07-speed/speed/game/input_service.py
+++ 07-speed/speed/game/input_service.py
@@ -33,6 +33,10 @@ class InputService:
         result = ""
         self.erase = False
         event = raylibpy.get_key_pressed()
+        if _is_enter_pressed():
+            event = 13
+        elif _is_esc_pressed():
+            event = 27
         assert type(event) is int
         
         if not event is None:
@@ -49,3 +53,9 @@ class InputService:
 
     def erase_function(self):
         return self.erase
+
+    def _is_enter_pressed(self):
+        return raylibpy.is_key_down(raylibpy.KEY_ENTER)
+
+    def _is_esc_pressed(self):
+        return raylibpy.is_key_down(raylibpy.KEY_ESCAPE)
